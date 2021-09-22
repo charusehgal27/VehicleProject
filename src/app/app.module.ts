789ffa6ce3cd04d834fcc3057120596ffe17136d
ameya1301999 <ameyarathi1301999@gmail.com>
2021-09-23 00:35:06 +0530

    updated the ngModule.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
-=======
+import { HttpClientModule } from '@angular/common/http';
 import { RegistrationComponent } from './registration/registration.component';
->>>>>>> 696e797c5006c14514fd274a244ca4d1ebcbb117
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    HomeComponent
-=======
+    HomeComponent,
     RegistrationComponent
->>>>>>> 696e797c5006c14514fd274a244ca4d1ebcbb117
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
